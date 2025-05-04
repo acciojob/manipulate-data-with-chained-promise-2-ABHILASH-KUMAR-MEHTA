855a3e32c8ab46c6c41b8c0c9b1fde10c5768f65
Abhilash Kumar Mehta <145607994+ABHILASH-KUMAR-MEHTA@users.noreply.github.com>
2025-05-04 23:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+})
+.then((data) => {
+  return new Promise((resolve) => {
+    const evens = data.filter(num => num % 2 === 0);
+    setTimeout(() => {
+      document.getElementById('output').innerText = evens.join(',');
+      resolve(evens);
+    }, 1000);
+  });
+})
+.then((evens) => {
+  return new Promise((resolve) => {
+    const doubled = evens.map(num => num * 2);
+    setTimeout(() => {
+      document.getElementById('output').innerText = doubled.join(',');
+      resolve(doubled);
+    }, 2000); 
+  });
+});
